b8f3445fcd01c3559f2a4ee94c9156eccf6d2c44
javedalihosamani <javed.hosamani007@gmail.com>
2024-09-16 16:13:05 +0530

    React Route is completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+import { ToastContainer } from "react-toastify";
+import Pnf from "./layout/Pnf";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <BrowserRouter>
+    <Header/>
+    <ToastContainer autoClose={3000} position={"top-right"}/>
+    <Routes>
+      <Route path={`/*`} element={<Pnf/>}/>
+    </Routes>
+    <Footer/>
+  </BrowserRouter>    
   );
 }
 
---
+++ src/layout/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-warning shadow'>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <p className='text-center text-white p-5'>All Rights Reserved By Javed Ali</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer;
